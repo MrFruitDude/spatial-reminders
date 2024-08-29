6b83e94ff0d59211a9b447276000256d6c6c2841
MrFruitDude <simon.roetzer@gmail.com>
2024-08-29 18:46:17 -0400

    Updates

---
+++ precache-manifest.45eaecff894b6f602db0a8d42d02020f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc7bba660d647713e9206e7f998fea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c818bd59b1a914e8a88",
+    "url": "/static/css/main.3cd1529c.chunk.css"
+  },
+  {
+    "revision": "6c019ebd9f0ab9bf9bdd",
+    "url": "/static/js/2.97947282.chunk.js"
+  },
+  {
+    "revision": "6a2a939c6c472481a460",
+    "url": "/static/js/3.5a11cc32.chunk.js"
+  },
+  {
+    "revision": "8c818bd59b1a914e8a88",
+    "url": "/static/js/main.654095eb.chunk.js"
+  },
+  {
+    "revision": "6d57d2952c711127ca6e",
+    "url": "/static/js/runtime~main.93c88974.js"
+  },
+  {
+    "revision": "9a660d0d80b12ca21b343f6a1ba05a2b",
+    "url": "/static/media/SR_featureWalkthrough_mobile_01.9a660d0d.mp4"
+  },
+  {
+    "revision": "09e2b08f624eda8f4c15fb71b6fa9b5b",
+    "url": "/static/media/SR_featureWalkthrough_v06_1x1_01.09e2b08f.mp4"
+  },
+  {
+    "revision": "42795b52c86a8986c0f5cb7eecc30c75",
+    "url": "/static/media/brain_icon_transparent.42795b52.png"
+  },
+  {
+    "revision": "b18362fe17be3f6be9bfe376cfab8e7f",
+    "url": "/static/media/calendar_icon_transparent.b18362fe.png"
+  },
+  {
+    "revision": "83587e2afd868b1a8d17365634bcc8d0",
+    "url": "/static/media/dragDropMain_02.83587e2a.mp4"
+  },
+  {
+    "revision": "27f883c0c1b374a4171bcabab666d801",
+    "url": "/static/media/dragDropMain_mobile.27f883c0.mp4"
+  },
+  {
+    "revision": "70c4468b88d42248faee75af52ed001f",
+    "url": "/static/media/filter_icon_transparent.70c4468b.png"
+  },
+  {
+    "revision": "0a7c779d8b3bc147348d476a21026733",
+    "url": "/static/media/folder_room_02.0a7c779d.png"
+  },
+  {
+    "revision": "3df614230ad713673582903014c26505",
+    "url": "/static/media/logo.3df61423.png"
+  },
+  {
+    "revision": "8f2651002123998f937159c491cbb33c",
+    "url": "/static/media/love_apple.8f265100.png"
+  },
+  {
+    "revision": "761bc8c8110050525bb8ea9261b33ad0",
+    "url": "/static/media/palette_icon_transparent.761bc8c8.png"
+  },
+  {
+    "revision": "59b377b3bfb6a89419354754af6a6ea1",
+    "url": "/static/media/pin_icon_transparent.59b377b3.png"
+  },
+  {
+    "revision": "c9447e2b29baac77bed52df2ddd324c8",
+    "url": "/static/media/puzzle_icon_transparent.c9447e2b.png"
+  },
+  {
+    "revision": "ba46f5b9de8617271ddc3c334d50641e",
+    "url": "/static/media/recurrencePatterns_01.ba46f5b9.jpg"
+  },
+  {
+    "revision": "c17729ab4e351733aad19727d590130c",
+    "url": "/static/media/sync_icon_transparent.c17729ab.png"
+  },
+  {
+    "revision": "7c637f6855af82b78bf867a17062a48c",
+    "url": "/static/media/widgetImage.7c637f68.jpg"
+  },
+  {
+    "revision": "a8e7a7c2e9c0321afd5a8966d2292747",
+    "url": "/static/media/widgetImage.a8e7a7c2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spatial-reminders/precache-manifest.d7bed5f37741500ba56d59ee7b4894da.js"
+  "/precache-manifest.45eaecff894b6f602db0a8d42d02020f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spatial-reminders/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
